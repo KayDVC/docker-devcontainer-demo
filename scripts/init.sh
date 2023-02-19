6a691b21affcd71829aef6871f3589b2e829d652
Malakai <kayydv1016@gmail.com>
2023-02-19 20:11:07 +0000

    Fixed a BUNCH of cross-comp bugs

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
-echo "---------------------------------"
-echo "Running startup script."
-task_list='prep_scripts '
+task_list='grant_ownership prep_scripts '
 tasks=$(echo "$task_list" | wc -w)
 completed=0
 
+echo "---------------------------------"
+echo "Logged in as: $(whoami)"
+echo "Running startup script; Remaining Tasks: ${tasks}"
+
+function grant_ownership() {
+    echo "Changing directory ownership from root to NRuser"
+    
+    sudo chown -R NRuser /home/NRuser
+
+    completed=$((completed+1))
+    display_progress
+}
 
 function prep_scripts (){
 
     echo "Granting execute perms for all scripts in folder."
 
-    for script in ./scripts/*; do chmod +x $script; done;
+    for script in ./scripts/*; do echo "Modifying $script " && chmod +x $script; done;
     
     completed=$((completed+1))
-
     display_progress
 }
 
@@ -26,4 +35,3 @@ for t in $task_list; do $t; done
 
 echo "---------------------------------"
 echo "Startup script completed."
-
